1761385d53c55d454d91b9786a1149ab1f7d97f3
animated-pancake <acads108@gmail.com>
2025-01-12 14:18:39 +0530

    change

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -2,7 +2,7 @@ import supabaseClient from "@/utils/supabase"
 
 export async function getJobs(token, {location, company_id, searchQuery}) {
     const supabase = await supabaseClient(token);
-
+// change
     let query = supabase.from("jobs").select("*");
 
         if(location){
