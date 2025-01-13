d67da45bb97d7f69f3c1ea8871504d4c923058a3
animated-pancake <acads108@gmail.com>
2025-01-13 22:15:12 +0530

    search and filter jobs

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -4,18 +4,24 @@ import useFetch from "../hooks/use-fetch"
 import { useUser } from "@clerk/clerk-react"
 import { BarLoader } from "react-spinners"
 import JobCard from "../components/job-card"
+import { Input } from "../components/ui/input"
+import { getCompanies } from "../api/apiCompanies"
+import { Button } from "../components/ui/button"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select" 
+import { State } from "country-state-city"
+
 
 
   const JobListing = () => {
     const [searchQuery, setSearchQuery] = useState(""); 
     const [location, setLocation] = useState(""); 
-    const [company_id, setCompanyId] = useState(""); 
+    const [company_id, setCompany_id] = useState(""); 
 
     const {isLoaded} = useUser();
 
     const {
       fn:fnJobs, 
-      data:jobs = [], 
+      data:jobs, 
       loading:loadingJobs
     } = useFetch(getJobs, {
       location, 
@@ -23,23 +29,85 @@ import JobCard from "../components/job-card"
       searchQuery,
     });
 
-    console.log("Jobs data:", jobs);
-
+    const {
+      fn:fnCompanies, 
+      data:companies, 
+    } = useFetch(getCompanies);
 
     useEffect(()=>{
-      if (isLoaded) {
-        
-        console.log("Fetching jobs with parameters:", { location, company_id, searchQuery }); fnJobs();
-      }
+      if (isLoaded) fnCompanies();
+    }, [isLoaded])
+
+    useEffect(()=>{
+      if (isLoaded) fnJobs();
     }, [isLoaded, location, company_id, searchQuery])
 
+    const handleSearch=(e)=>{
+      e.preventDefault();
+      let formData = new FormData(e.target)
+      
+      const query = formData.get("search-query")
+      if (query) setSearchQuery(query);
+    }
+
+      const clearFilters = () => {
+        setSearchQuery("");
+        setCompany_id("");
+        setLocation("");
+      }
     if(!isLoaded) {
       return <BarLoader className="mb-4" width={"100%"} color="#36d7b7"/>
     }
-  return (<div>
+  return (
+  <div>
     <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">Latest Jobs</h1>
 
     {/* Add filters here */}
+    <form onSubmit={handleSearch} className="h-14 flex flex-row w-full gap-2 items-center mb-3">
+      <Input type="text" placeholder="Search Jobs by Title.." name="search-query" className="h-full flex-1 px-4 text-md"/>
+      <Button type="submit" className="h-full sm:w-28" variant="blue">
+        Search
+      </Button>
+    </form>
+
+    <div className="flex flex-col sm:flex-row gap-2">
+    <Select value={location} onValueChange={(value) => setLocation(value)}>
+      <SelectTrigger>
+        <SelectValue placeholder="Filter by Location" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+        {State.getStatesOfCountry("IN").map(({name})=>{
+          return (
+          <SelectItem key={name} value={name}>
+          {name}
+        </SelectItem>
+        )
+      })}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+
+    <Select value={company_id} onValueChange={(value) => setCompany_id(value)}>
+      <SelectTrigger>
+        <SelectValue placeholder="Filter by Company" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {companies?.map(({ name, id}) => {
+            return (
+              <SelectItem key={name} value={id}>
+              {name}
+              </SelectItem>
+            )
+          })}          
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+    <Button onClick={clearFilters} variant="destructive" className="sm:w-1/2">
+    Clear Filters
+    </Button>
+    </div>
 
     {loadingJobs && (
       <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
@@ -49,7 +117,8 @@ import JobCard from "../components/job-card"
       <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         {jobs?.length ? (
           jobs.map((job)=>{
-            return <JobCard key={job.id} job={job} />
+            return ( <JobCard key={job.id} job={job} savedInit={job?.saved?.length > 0} />
+            )
           })
         ): (
           <div>No Jobs Found 😢</div>
